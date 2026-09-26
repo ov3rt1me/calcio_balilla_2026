fd5ef00e99dc86aed6e18606dfc88e0f521dc7b5
ov3rt1me <eguarato@gmail.com>
2026-09-26 11:18:27 +0200

    cacnella tutto

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// base './' + HashRouter => funziona su GitHub Pages in qualsiasi sottocartella
-export default defineConfig({
-  base: './',
-  plugins: [react(), tailwindcss()],
-})
